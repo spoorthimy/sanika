6a6c16a5aacdcbd03446f2dd63dd98fd62373f91
spoorthimy <163995471+spoorthimy@users.noreply.github.com>
2024-03-19 14:39:12 +0530

    Add files via upload

---
+++ cse (4).java
@@ -0,0 +1,32 @@
+class demo
+{
+public static void main(String args[])
+{
+int a,b,c;
+c=30;
+a=1272420;
+b=20;
+if(a>b)
+{
+if(a>c)
+{
+System.out.println("a is biggest");
+}
+else
+{
+System.out.println("b is biggest");
+}
+}
+else
+{
+if(b>c)
+{
+System.out.println("b is biggest");
+}
+else
+{
+System.out.println("c is biggest");
+}
+}
+}
+}
